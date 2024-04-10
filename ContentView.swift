0bac62d158a53b1d75746dbe72eaa3c22d3f2c0e
Evan Gross <egross6142@stu.d214.org>
2024-04-10 14:35:44 -0500

    textfield var 2

--- ContentView.swift
+++ ContentView.swift
@@ -5,11 +5,6 @@ struct ContentView: View {
     @State var number2 = 0
     var body: some View {
         
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        TextField("Textfield", value: $number2, format: .number)
     }
 }
