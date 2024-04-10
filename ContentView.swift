4013e36598e115059ef232b6385cd7e1aa811b72
Evan Gross <egross6142@stu.d214.org>
2024-04-10 14:47:39 -0500

    text + var

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,14 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
+    @State var answer = "Answer is "
     var body: some View {
-        TextField("yo", value: $number2, format: .number)
+        TextField("Textfield", value: $number2, format: .number)
+        
+        TextField("yo", value: $number1, format: .number)
+        
+        Text("\(answer)")
+        
+        
     }
 }
