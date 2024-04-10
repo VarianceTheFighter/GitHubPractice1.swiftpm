9ff8cad6cbb54ed17baeab013bf3b6c32accb2d0
Allan Sulev <asulev7401@stu.d214.org>
2024-04-10 14:52:48 -0500

    Modifired button and textfield

--- ContentView.swift
+++ ContentView.swift
@@ -3,17 +3,21 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
-    @State var answer = "Answer is "
+    @State var answer = 0
     var body: some View {
         TextField("Textfield", value: $number2, format: .number)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
         
         TextField("yo", value: $number1, format: .number)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+        
         Button(action: {
             
             answer = number1 + number2
             
         }, label: {
             Text("Add")
+                .foregroundStyle(.blue)
         })
         
         Text("\(answer)")
