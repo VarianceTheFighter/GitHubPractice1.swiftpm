1c628469dbcd71e7de83145e4100398de5b736d3
Allan Sulev <asulev7401@stu.d214.org>
2024-04-10 14:45:39 -0500

    Made them Add

--- ContentView.swift
+++ ContentView.swift
@@ -4,6 +4,14 @@ struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
     var body: some View {
-        TextField("yo", value: $number2, format: .number)
+        TextField("TextField", value: $number2, format: .number)
+        TextField("yo", value: $number1, format: .number)
+        Button(action: {
+            
+            answer = number1 + number2
+            
+        }, label: {
+            Text("Add")
+        })
     }
 }
